7a3a463da9fc9fac79ee7cb9aa02cb76450e7f61
VPKSoft <vpksoft@vpksoft.net>
2020-12-17 18:00:46 +0200

    Added NET Core support package.

--- VPKSoft.SpellCheck.ExternalDictionarySource/Properties/AssemblyInfo.cs
+++ VPKSoft.SpellCheck.ExternalDictionarySource/Properties/AssemblyInfo.cs
@@ -28,18 +28,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("VPKSoft.SpellCheck.ExternalDictionarySource")]
-[assembly: AssemblyDescription("External dictionary source for Interface for VPKSoft.SpellCheckUtility and VPKSoft.ScintillaSpellCheck class libraries.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VPKSoft")]
-[assembly: AssemblyProduct("VPKSoft.SpellCheck.ExternalDictionarySource")]
-[assembly: AssemblyCopyright("Copyright © VPKSoft 2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -47,16 +35,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("11881773-ca29-4242-b1e1-6ce26a47164f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
